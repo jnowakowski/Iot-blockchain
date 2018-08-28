714fd62baa22df9c11f869dfc6eb4ae349cd0f7e
Marcin <gierlak@gmail.com>
2018-08-28 19:43:57 +0200

    refactor

--- eth_call.ino
+++ eth_call.ino
@@ -9,13 +9,18 @@ const string CONTRACT_ADDRESS = "0xdeb1331a4b43839ad427d81ebee5ce915171e925";
 const char* ACCOUNT_ADDRESS = "0xdce18b3f54c1ef557d48b98d3521aeda35ccc848";
 const string INFURA_HOST = "ropsten.infura.io";
 const string INFURA_PATH = "/YOUR_PATH";
+string result = "";
 
 int relayPin = 5;
 int buzzerPin = 4;
 int lastState = 0;
+long previousMillis = 0;
+long interval = 20000;
+
 
 Web3 web3(&INFURA_HOST, &INFURA_PATH);
 WiFiServer server(80);
+Contract contract(&web3, &CONTRACT_ADDRESS);
 
 void eth_call();
 
@@ -45,22 +50,43 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(WiFi.localIP());
-    delay(2000);
-    eth_call();
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis > interval)
+    { 
+      eth_call();
+      previousMillis = currentMillis;   
+    }
+    wifi_server();
 }
 
-void eth_call() {
-    Contract contract(&web3, &CONTRACT_ADDRESS);
+void wifi_server()
+{
+   WiFiClient client = server.available();
+    if (client) {
+      while (client.connected()) {
+      Serial.println(" ## web client conneted.");
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-type:text/html");
+      client.println("Connection: close");
+      client.println("");
+      client.println("Contract address: ");
+      client.print(CONTRACT_ADDRESS.c_str());
+      client.println("<br>Contract value: ");
+      client.print(result.c_str());
+      client.stop();
+    }}
+}
 
+void eth_call() {  
+    Serial.println(WiFi.localIP());
     strcpy(contract.options.from, ACCOUNT_ADDRESS);
     strcpy(contract.options.gasPrice,"2000000000000");
     contract.options.gas = 5000000;
     string func = "isActive()";
     string param = contract.SetupContractData(&func);
-    string result = contract.Call(&param);
-    char value = result[result.length()-4];
-
+    result = contract.Call(&param);
+    char value = result[result.length()-3];
+    Serial.println(value);
     Serial.println(lastState);
     if(value == '1' && lastState == 0)
     {
@@ -80,16 +106,4 @@ void eth_call() {
       lastState = 0;
       Serial.println("Switch OFF");
     }
-
-    WiFiClient client = server.available();
-    if (client) {
-      Serial.println(" ## web client conneted.");
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-type:text/html");
-      client.println("Connection: close");
-      client.println("");
-      client.println("Contract address: %s", CONTRACT_ADDRESS);
-      client.println("Contract value: %s", value);
-    }
-    client.stop();
 }
