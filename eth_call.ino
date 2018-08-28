e289761f2b2490b167131dd58ff5675985f36be0
Unknown <gierlak@gmail.com>
2018-08-28 13:06:29 +0200

    added relay module control

--- eth_call.ino
+++ eth_call.ino
@@ -5,16 +5,19 @@
 #define USE_SERIAL Serial
 #define ENV_SSID     "Krakow"
 #define ENV_WIFI_KEY "8.warszawa.8"
-const string CONTRACT_ADDRESS = "0xb9bb5ad4fb18026435cfc377802f4529e47cc356";
+const string CONTRACT_ADDRESS = "0x13249c8503f3b4a9974f02cc61254df62d6e8a94";
 const char* ACCOUNT_ADDRESS = "0xdce18b3f54c1ef557d48b98d3521aeda35ccc848";
 const string INFURA_HOST = "ropsten.infura.io";
 const string INFURA_PATH = "/YOUR_PATH";
+int relayPin = 5;
 
 Web3 web3(&INFURA_HOST, &INFURA_PATH);
 
-void eth_call_example();
+void eth_call();
 
 void setup() {
+    pinMode(relayPin, OUTPUT);
+    digitalWrite(relayPin, LOW);
     USE_SERIAL.begin(115200);
 
     for(uint8_t t = 4; t > 0; t--) {
@@ -46,8 +49,18 @@ void eth_call() {
     strcpy(contract.options.from, ACCOUNT_ADDRESS);
     strcpy(contract.options.gasPrice,"2000000000000");
     contract.options.gas = 5000000;
-    string func = "readUsersLog()";
+    string func = "isActive()";
     string param = contract.SetupContractData(&func);
     string result = contract.Call(&param);
-    USE_SERIAL.println(result.c_str());
+    char value = result[result.length()-4];
+    if(value == '1')
+    {
+      digitalWrite(relayPin, HIGH);
+      USE_SERIAL.println("Switch ON");
+    }
+    else
+    { 
+      digitalWrite(relayPin, LOW);
+      USE_SERIAL.println("Switch OFF");
+    }
 }
